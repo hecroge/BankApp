692ccdd9dea73df2b2562f69101f502d208bea2c
Héctor Rogel <hrogel@izertis.com>
2023-04-03 20:12:42 +0200

    BankApp-1: first commit, directories structure

---
+++ BankApp/BankApp/Platform/App/BankApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankApp.swift
+//  BankApp
+//
+//  Created by Héctor Rogel on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct BankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BankApp/BankApp/Platform/Scenes/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BankApp
+//
+//  Created by Héctor Rogel on 3/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
