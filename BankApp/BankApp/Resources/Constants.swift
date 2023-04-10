9c9c5952bfc3237574ff3b162130b09f40e6ec5a
hecroge <hector.rogeldelgado@gmail.com>
2023-04-10 18:55:12 +0200

    BankApp-4: removed unused code and folders, strings into constants files

--- BankApp/BankApp/Resources/Constants.swift
+++ BankApp/BankApp/Resources/Constants.swift
@@ -9,4 +9,6 @@ import Foundation
 
 struct Constants {
     static let baseUrl = "https://code-challenge-e9f47.web.app"
+    static let serverDateFormat = "yyyy-MM-dd'T'HH:mm:ss.sssZ"
+    static let appDateFormat = "dd/MM/YYYY"
 }
